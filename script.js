d40eb4ddb189f02894708b427943f66573599d0d
abrahamm <abrahamm8710@gmail.com>
2026-04-11 20:32:32 -0600

    updated files
    reverted a change but still keep loading animation

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 window.addEventListener("load", function() {
   const loader = document.getElementById("loader");
 
-  // Only show loader and redirect if this is the first visit
   if (!sessionStorage.getItem("loaderShown")) {
     sessionStorage.setItem("loaderShown", "true");
-    
+
     setTimeout(() => {
       loader.style.opacity = "0";
       loader.style.pointerEvents = "none";
-      window.location.href = "index.html";
-    }, 2000);
+    }, 4000);
   } else {
-    // On subsequent visits, hide loader immediately
     loader.style.opacity = "0";
     loader.style.pointerEvents = "none";
   }
