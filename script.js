9b5d583cb3d6d007a29d99e26ecd69782d650b2e
abrahamm <abrahamm8710@gmail.com>
2026-04-11 20:02:25 -0600

    update index.html script.js style.css
    added a loading bar animation to my website testing to see if it works

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.pointerEvents = "none";
+  }, 2000);
+});
