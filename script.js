79fdca79ce50deb0213efcc7d6a44b21d2368be4
abrahamm <abrahamm8710@gmail.com>
2026-04-11 20:19:48 -0600

    updated multiple files
    made multiple changes

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 window.addEventListener("load", function() {
   const loader = document.getElementById("loader");
 
-  setTimeout(() => {
+  // Only show loader and redirect if this is the first visit
+  if (!sessionStorage.getItem("loaderShown")) {
+    sessionStorage.setItem("loaderShown", "true");
+    
+    setTimeout(() => {
+      loader.style.opacity = "0";
+      loader.style.pointerEvents = "none";
+      window.location.href = "index.html";
+    }, 2000);
+  } else {
+    // On subsequent visits, hide loader immediately
     loader.style.opacity = "0";
     loader.style.pointerEvents = "none";
-    window.location.href = "index.html";
-  }, 2000);
+  }
 });
